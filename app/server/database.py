2a4e039672395a3a705dd459b45e79fb18846a0d
Bartek1995 <bartek.swatek@wp.pl>
2022-07-03 13:45:33 +0200

    Add database feature to get data for charts

--- app/server/database.py
+++ app/server/database.py
@@ -89,7 +89,9 @@ class DatabaseManager:
                 return self.borrowing_books_collection.find(collection_of_arguments_to_search_in_db)
 
     def generate_card_number(self):
-
+        """
+        Function searching for highest "card_number" value in collection and returns incremented value.
+        """
         card_number = 1
 
         reader_list = self.readers_collection.find()
@@ -101,7 +103,10 @@ class DatabaseManager:
         return card_number + 1
 
     def get_values_and_days_to_statistics_chart(self, start_date, end_date):
-
+        """
+        Returns days and values of book borrowing.
+        You can pass start date and end date as parameter to select date range.
+        """
         values = []
         days = []
 
@@ -121,24 +126,64 @@ class DatabaseManager:
         return {'values': values,
                 'days': days}
 
-
     def get_readers_and_amount_of_books_to_statistics_chart(self):
+        """
+        Returns names of readers and amount of borrowed books as dictionary.
+        """
         reader_name = []
         amount_of_borrowed_books = []
 
         temp_reader_list = list(self.readers_collection.find({'book_borrowing_counter': {"$gt": 0}}))
-        newlist = sorted(temp_reader_list, reverse=True, key=itemgetter('book_borrowing_counter'))
+        reader_list = sorted(temp_reader_list, reverse=True, key=itemgetter('book_borrowing_counter'))
 
-        for reader in newlist[:5]:
+        for reader in reader_list[:5]:
             full_name = f"{reader['reader_first_name']} {reader['reader_second_name']}"
             reader_name.append(full_name)
             amount_of_borrowed_books.append(reader['book_borrowing_counter'])
         return {'reader_name': reader_name,
-                'amount_of_borrowed_books': amount_of_borrowed_books }
+                'amount_of_borrowed_books': amount_of_borrowed_books}
+
+    def get_most_frequently_borrowed_books(self):
+        """
+        Returns books titles and amount of books borrowing as dictionary.
+        """
+        book_titles = []
+        amount = []
+
+        temp_book_list = list(self.books_collection.find({'book_borrowing_counter': {"$gt": 0}}))
+        book_list = sorted(temp_book_list, reverse=True, key=itemgetter('book_borrowing_counter'))
+
+        for book in book_list[:6]:
+            if len(book['title']) > 20:
+                splitted_book_name = book['title'].split()
+                book_titles.append(splitted_book_name)
+            else:
+                book_titles.append(book['title'])
+
+            amount.append(book['book_borrowing_counter'])
+
+        return {
+                'book_titles': book_titles,
+                'amount': amount,
+        }
+
+    def get_percent_of_finished_book_borrowings(self):
+        """
+        Returns number of finished and unfinished book borrowing.
+        """
+        unfinished_borrowing_book_amount = self.borrowing_books_collection.count_documents({'is_finished': False})
+        finished_borrowing_book_amount = self.borrowing_books_collection.count_documents({'is_finished': True})
+
+        return {
+            'borrowing_books_amount_as_percentage': [unfinished_borrowing_book_amount, finished_borrowing_book_amount]
+        }
 
     def export_collection_as_json(self, collection_name):
+        """
+        Export collection as json file.
+        You have to pass collection name as parameter.
 
-        print(collection_name)
+        """
 
         match collection_name:
             case 'borrowing_book_collection':
@@ -177,7 +222,10 @@ class DatabaseManager:
                     file.write(json_data)
 
     def import_collection(self, file, collection_name):
-
+        """
+        Import given collection JSON file to database.
+        The function completes the missing documents in selected collection.
+        """
         json_data = json.load(file.file)
 
         for jsonObj in json_data:
