d1649351eeaa78aadcf01ebe970538ce395cfb5b
Bartek1995 <bartek.swatek@wp.pl>
2022-07-05 17:36:33 +0200

    Change full structure of project(Heroku deploy problem fix): - Changed directories - Changed imports and it's names - Changed files paths

--- app/static/js/js.js
+++
@@ -1,8 +0,0 @@
-const close_button = document.querySelector(".btn-close");
-
-if (close_button != null) {
-  close_button.addEventListener("click", () => {
-    const alert_div = document.querySelector(".alert");
-    alert_div.classList.toggle("hide");
-  });
-}
